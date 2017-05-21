7852ac2a1f45f50c75459ff150743b9b1a51aba1
Kyle <kylerperks@gmail.com>
2017-05-20 20:23:15 -0400

    Initial Commit

---
+++ euler_problem_01.js
@@ -0,0 +1,13 @@
+/*
+  euler problem 1 
+  sum the multiples of 3 and 5 from 1-1000
+*/
+
+var sum = 0;
+for(var i = 0; i < 1000; i++) {
+  if(i % 3 === 0 || i % 5 === 0) {
+    sum += i;
+  }
+}
+
+console.log(sum); 
